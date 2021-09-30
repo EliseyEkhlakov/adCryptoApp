a3ff220dba1b9daf0537b47f2f57faab07bba0aa
Elisey Ekhlakov <ekhalakovea@bk.ru>
2021-09-30 23:16:59 +0400

    +success get data from network

---
+++ app/src/main/java/com/adorly/cryptoapp/api/ApiService.kt
@@ -0,0 +1,36 @@
+package com.adorly.cryptoapp.api
+
+import com.adorly.cryptoapp.pojo.CoinInfoListOfData
+import com.adorly.cryptoapp.pojo.CoinPriceInfoRawData
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+import java.util.*
+
+interface ApiService {
+    @GET("top/totalvolfull")
+    fun getTopCoinsInfo(
+        @Query(QUERY_PARAM_API_KEY) apiKey:String = API_KEY,
+        @Query(QUERY_PARAM_LIMIT) limit:Int = 10,
+        @Query(QUERY_PARAM_TO_SYMBOL) tSym:String = CURRENCY
+    ): Single<CoinInfoListOfData>
+
+    @GET("pricemultifull")
+    fun getFullPriceList(
+        @Query(QUERY_PARAM_API_KEY) apiKey:String = API_KEY,
+        @Query(QUERY_PARAM_FROM_SYMBOLS) fSyms:String,
+        @Query(QUERY_PARAM_TO_SYMBOLS) tSyms:String = CURRENCY,
+    ):Single<CoinPriceInfoRawData>
+
+    companion object{
+        private const val QUERY_PARAM_API_KEY = "api_key"
+        private const val QUERY_PARAM_LIMIT = "limit"
+        private const val QUERY_PARAM_TO_SYMBOL = "tsym"
+
+        private const val QUERY_PARAM_TO_SYMBOLS = "tsyms"
+        private const val QUERY_PARAM_FROM_SYMBOLS = "fsyms"
+
+        private const val CURRENCY = "USD"
+        private const val API_KEY = "6af2ac73332d1d82c4202c498cdb3b8235238bb4fd417d9442968c84a283c08f"
+    }
+}
