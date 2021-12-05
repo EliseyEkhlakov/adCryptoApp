bfc49adae0062d93ab73f443f9ad0411bf6d958e
Elisey Ekhlakov <ekhalakovea@bk.ru>
2021-12-05 20:11:01 +0400

    +view binding in adapter

---
+++ app/src/main/java/com/adorly/cryptoapp/presentation/adapters/CoinInfoViewHolder.kt
@@ -0,0 +1,8 @@
+package com.adorly.cryptoapp.presentation.adapters
+
+import androidx.recyclerview.widget.RecyclerView
+import com.adorly.cryptoapp.databinding.ItemCoinInfoBinding
+
+class CoinInfoViewHolder(
+    val binding: ItemCoinInfoBinding
+) : RecyclerView.ViewHolder(binding.root)
